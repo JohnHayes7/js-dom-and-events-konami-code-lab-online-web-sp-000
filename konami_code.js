fa8b66a17bd1805dade24d67f5fcab51824f7c62
John Hayes <JohnnyHayes7@gmail.com>
2019-09-04 10:25:54 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let i = 0
 function init() {
   // Write your JavaScript code inside the init() function
+  document.addEventListener('keydown', function(e){
+    const key = parseInt(e.which || e.detail);
+    // console.log(key)
+    if (key === code[i]){
+      // console.log(key)
+      i++;
+      if (i === code.length){
+        alert("You entered the Konami code!");
 
+        i = 0
+      }
+    }else{
+      i = 0
+    };
+  });
 }
+
+// init();
+
